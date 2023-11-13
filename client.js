f44fa5ee87b666639e74bdaaec186317f0493c70
LuAnn Yang <luann.yang@gmail.com>
2023-11-13 16:47:07 -0600

    Finished Homework

--- client.js
+++ client.js
@@ -7,6 +7,44 @@
  *    hobbies
  */
 
+hobbies = ['sky diving', 'nature', 'fishing', 'boating'];
+i = 0;
+while (i < hobbies.length){
+     console.log(hobbies[i]); i++;
+}
+
+console.log('hobbies length', hobbies.length);
+
+
+//PRACTICE 
+
+// hobbies = ['basketball', 'running', 'climbing', 'cavediving'];
+// i = 0;
+
+// while (i <hobbies.length){
+//      console.log(hobbies[i]); i++;
+// }
+
+
+
+// hobbies = ['music', 'adventure', 'game'];
+// i = 0;
+// while (i<hobbies.length){
+//      console.log(hobbies[i]); i++;
+// }
+
+//PRACTICE
+
+// let hobbies = ['hiking', 'partying', 'sleeping'];
+// console.log(hobbies);
+
+// let i= 0;
+
+// while(i<hobbies.length){
+//      console.log(hobbies[i]); i++;
+// }
+// console.log(hobbies.length);
+
 
 // Example output
 // 1. swimming
@@ -25,6 +63,49 @@
  * 4. Output the array and number of times teal was found
  */
 
+const colors = ['red', 'purple', 'grey', 'teal', 'teal', 'teal'];
+let tealCount = 0;
+i = 0;
+
+for (i; i < colors.length; i++){
+     if (colors[i] === 'teal'){ // the { } tells you the beginning of the block code that should be run if this condition is true.
+          tealCount++; // this counts the teal and increments by 1
+     }// this ends the "to do something" if the color is teal
+}
+
+console.log('Teal Count', tealCount);
+
+//PRACTICE
+
+// const colors = 'red'; 'blue'; 'green';
+// let tealCount = 0;
+// i = 0;
+
+// for (i= 0; i < colors.length; i++){
+//      if(colors[i] === 'teal');
+//      tealCount++; //if the condition is true, increment teal count by 1
+// }
+
+// console.log('Teal Count', tealCount);
+
+//PRACTICE
+
+// let colors = ['red', 'teal', 'green'];
+
+// let tealCount = 0;
+
+// i = 0;
+
+// while (i < colors.length){
+//      if (colors[i] === 'teal'){
+//           tealCount++;
+
+//      };
+// i++
+// }
+
+// console.log(tealCount);
+
 
 // Example output
 // green, red, teal, orange, teal
@@ -36,10 +117,69 @@
  * -------------
  * 1. Create an array of numbers (at least 5 numbers)
  * 2. Create variables oddNumbers and evenNumbers (empty arrays)
- * 3. Write a loop that puts all the odd numbers in the oddNumbers 
+ * 3. Write a loop that puts all the odd numbers in the oddNumbers
  *    array and even numbers in the evenNumbers array.
  * 4. Output the original array, odd number array and even number array
  */
+
+const numbers = [0, 4, 6, 3, 1, 10, 67, 198];
+let oddNumbers = [];
+let evenNumbers = [];
+
+console.log(numbers);
+
+for (i = 0; i < numbers.length; i++){
+     if (numbers[i] % 2 !== 0){
+          oddNumbers.push(numbers[i]); //uses .push(numbers[i]) vs oddNumbers++ because odd numbers is an array, not numerical value
+     }
+     else {
+          evenNumbers.push(numbers[i]);
+     }
+}
+
+console.log('Odd Numbers', oddNumbers);
+console.log('Even Numbers', evenNumbers);
+
+///PRACTICE///
+
+// const numbers = [3, 0, 2, 9, 1, 8];
+// let oddNumbers= [];
+// let evenNumbers= [];
+
+// for (i = 0; i < numbers.length; i++){
+//      if (numbers[i] % 2 === 0){
+//           evenNumbers++;
+//      }
+//      else {
+//           oddNumbers++;
+//      }
+// }
+
+// console.log('Even Numbers', evenNumbers);
+// console.log('Odd Numbers', oddNumbers); 
+
+//PRACTICE
+// let numbers = [1, 5, 8, 0];
+
+// let oddNumbers = [];
+// let evenNumbers = [];
+
+// let number = 4; 
+// console.log(number%2);
+
+// i = 0;
+
+// while (i < numbers.length){
+//      if (numbers[i] % 2 === 1){
+//          oddNumbers.push(numbers[i]);
+//      }
+//      console.log(oddNumbers);
+//      else {
+//           evenNumbers.push(numbers[i]);
+//       }
+//       console.log(evenNumbers);
+// i++
+// }
 
 
 // Example output
@@ -58,6 +198,49 @@
  */
 
 
+const boolean = [true, false, false, true, false, false];
+let toggled = []
+
+for (i = 0; i < boolean.length; i++){
+     toggled.push(!boolean[i]); //! means not. you are putting these in the empty toggled array
+     }                             // notice we didn't use push++ because it's not a count. 
+
+
+console.log('Boolean', boolean);
+console.log('Opposite Values', toggled);
+
+
+//PRACTICE
+// let boolean = [true,false,false,true,true];
+// let toggled = [];
+// i = 0;
+
+// for(i =0; i<boolean.length; i++){
+
+//      if (boolean[i] === true){
+//         console.log(boolean[i]);
+//         toggled.push(boolean[i])
+//   }
+//   console.log(toggled);
+// }
+
+
+
+// let boolean = [true,false,false,true,true];
+// let toggled = [];
+// i = 0;
+
+// for(i =0; i<boolean.length; i++){
+
+//      if (boolean[i] === true){
+//         console.log(boolean[i]);
+//         toggled.push('false')
+//   }
+//      else {
+//           toggled.push('true');
+//      }
+//   console.log(toggled);
+// }
 // Example output
 // true, false, true, true
 // Toggled false, true, false, false
@@ -75,6 +258,16 @@
  * Hint: Try using a while loop for this one.
  */
 
+const numbers2 = [0, 10, 39, 124, 30, 15, 120, 0, 0];
+i = 0;
+console.log('Normal numbers list', numbers2);
+
+while (numbers2 [numbers2.length -1] === 0){
+     numbers2.pop();
+}
+
+
+console.log('Numbers popped', numbers2);
 
 // Example output
 // Before loop 3, 0, 2, 8, 0, 0, 0
